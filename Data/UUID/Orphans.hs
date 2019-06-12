a4e70d1cbb01573fdf2dea55a31f166324400fcf
David Fox <dsf@seereason.com>
2019-06-12 11:46:25 -0700

    Changes for safecopy-0.9.5

--- Data/UUID/Orphans.hs
+++ Data/UUID/Orphans.hs
@@ -1,22 +1,33 @@
-{-# LANGUAGE CPP, TemplateHaskell #-}
+{-# LANGUAGE CPP, DeriveGeneric #-}
+#if MIN_VERSION_safecopy(0,9,5)
+{-# DeriveLift, StandaloneDeriving #-}
+#else
+{-# LANGUAGE TemplateHaskell #-}
+#endif
+
 module Data.UUID.Orphans (showUUID) where
 
-import Data.SafeCopy (base, deriveSafeCopy)
+import Data.SafeCopy (SafeCopy(..))
 import Data.Text as T (pack, unpack)
-import Data.UUID.Types (UUID, toString, fromString)
-import Language.Haskell.TH.Lift (deriveLiftMany)
+import Data.UUID.Types (toString, fromString)
+import Data.UUID.Types.Internal (UUID(..))
+import Language.Haskell.TH.Lift (Lift)
 import Web.Routes.PathInfo
 
+deriving instance Generic UUID
+
+#if MIN_VERSION_safecopy(0,9,5)
+instance SafeCopy UUID where version = 0
+deriving instance Lift UUID
+#else
 $(deriveSafeCopy 0 'base ''UUID)
+$(deriveLiftMany [''UUID])
+#endif
 
 instance PathInfo UUID where
   toPathSegments = (:[]) . T.pack . toString
   fromPathSegments = pToken (const ("UUID" :: String)) checkUUID
     where checkUUID txt = fromString (T.unpack txt)
-
-$(deriveLiftMany [
-   ''UUID
-  ])
 
 -- | The Show instance for UUID does not return a string containing a
 -- haskell expression, so if that is required use this function instead.
