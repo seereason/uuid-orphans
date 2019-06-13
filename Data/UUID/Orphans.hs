27952f4ec88de6f2b3459e79953031c011f7ee23
David Fox <dsf@seereason.com>
2019-06-12 20:03:21 -0700

    Changes for safecopy-0.9.5

--- Data/UUID/Orphans.hs
+++ Data/UUID/Orphans.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE CPP, DeriveGeneric #-}
+{-# LANGUAGE CPP, DeriveGeneric, DeriveLift #-}
 #if MIN_VERSION_safecopy(0,9,5)
-{-# LANGUAGE DeriveLift #-}
 {-# LANGUAGE StandaloneDeriving #-}
 #else
 {-# LANGUAGE TemplateHaskell #-}
@@ -8,7 +7,7 @@
 
 module Data.UUID.Orphans (showUUID) where
 
-import Data.SafeCopy (SafeCopy(..))
+import Data.SafeCopy -- (base, contain, deriveSafeCopy, SafeCopy(..))
 import Data.Text as T (pack, unpack)
 import Data.UUID.Types (toString, fromString)
 import Data.UUID.Types.Internal (UUID(..))
@@ -19,11 +18,34 @@ deriving instance Generic UUID
 
 #if MIN_VERSION_safecopy(0,9,5)
 instance SafeCopy UUID where version = 0
-deriving instance Lift UUID
 #else
 $(deriveSafeCopy 0 'base ''UUID)
-$(deriveLiftMany [''UUID])
 #endif
+
+#if 0
+-- Splices
+instance SafeCopy UUID where
+      putCopy (UUID a1 a2 a3 a4)
+        = contain
+            (do safePut_Word32 <- getSafePut
+                safePut_Word32 a1
+                safePut_Word32 a2
+                safePut_Word32 a3
+                safePut_Word32 a4
+                return ())
+      getCopy
+        = contain
+            ((label "Data.UUID.Types.Internal.UUID:")
+               (do safeGet_Word32 <- getSafeGet
+                   ((((return UUID <*> safeGet_Word32) <*> safeGet_Word32)
+                       <*> safeGet_Word32)
+                      <*> safeGet_Word32)))
+      version = 0
+      kind = base
+      errorTypeName _ = "Data.UUID.Types.Internal.UUID"
+#endif
+
+deriving instance Lift UUID
 
 instance PathInfo UUID where
   toPathSegments = (:[]) . T.pack . toString
